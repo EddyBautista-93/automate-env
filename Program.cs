864971be3484476ef52bc78f17ce54df67c8e9df
EddyBautista <bautista.j.eddy@gmail.com>
2022-06-14 19:58:08 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace automate_env
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
